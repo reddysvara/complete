5ec994bce99c298840046a9b151500b2508c707a
varakanthreddypatllolla <varakanthreddypatllolla@192.168.0.21>
2017-02-24 19:08:56 -0600

    Firt Commit

---
+++ demo/src/main/java/com/clone/demo/MainDemo.java
@@ -0,0 +1,12 @@
+package com.clone.demo;
+
+public class MainDemo {
+
+	public static void main(String[] args) {
+		
+		System.out.println("Sample class 1");
+		System.out.println("Sample class 2");
+
+	}
+
+}
